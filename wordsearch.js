d18b4da63c54859d67cb0d00e9879a6c2250fef4
Lily Go <lilygo7192@gmail.com>
2023-03-15 16:48:34 -0700

    Implemented backwards and horizontal testing

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,53 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  // create an empty array
+  const result = [];
+
+  // loop matrix[i].length number of times 
+  for (let i = 0; i < matrix[0].length; i++) {
+    // create an empty array for each loop
+    let arr = [];
+    
+    // loop through the numbers inside the nested arrays 
+    for (const element of matrix) {
+      // transpose the numbers that correspond to variable num
+      arr.push(element[i]);
     }
+
+    // push arr to final results array
+    result.push(arr);
+  }
+
+  return result;
+};
+
+const wordSearch = (letters, word) => {
+    if (letters.length === 0){
+      return false;
+    }
+  
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    };
+
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    for (l of verticalJoin) {
+      if (l.includes(word)) return true;
+    };
+    
+    const horizontalReverse = letters.map(ls => ls.reverse().join(''));
+    for (l of horizontalReverse) {
+      if (l.includes(word)) return true;
+    };
+
+    const verticalReverse = transpose(letters).map(ls => ls.reverse().join(''));
+    for (l of verticalReverse) {
+      if (l.includes(word)) return true;
+    };
+
+
+
+    return false;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
